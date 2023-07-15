05f99248baf618345568077f9f9b4678329f7083
indira-faizullina <indiramus@list.ru>
2023-07-15 13:35:29 +1100

    burger-menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-// плавная прокрутка меню
-
 const anchors = document.querySelectorAll('li>a[href*="#"]')
+const burgerMenu = document.querySelector('#burger-menu')
+const burgerMenuToggle = document.querySelector('.menu__burger')
+
+// плавная прокрутка при нажатии на пункт в меню
 
 anchors.forEach((anchor) => {
   anchor.addEventListener('click', (event) => {
@@ -8,6 +10,7 @@ anchors.forEach((anchor) => {
 
     removeActiveClass(anchors)
     anchor.closest('li').classList.add('active')
+    burgerMenu.classList.toggle('open')
 
     const targetBlockId = anchor.getAttribute('href')
     if (targetBlockId === '#') return
@@ -22,3 +25,9 @@ anchors.forEach((anchor) => {
 function removeActiveClass(arr) {
   arr.forEach((elem) => elem.closest('li').classList.remove('active'))
 }
+
+// бургер меню
+
+burgerMenuToggle.addEventListener('click', () => {
+  burgerMenu.classList.toggle('open')
+})
